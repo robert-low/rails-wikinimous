fe5044c7e2f5bf2f1e82523d5a5eddc79cc5449a
Robert Low <robert.low91@gmail.com>
2023-03-23 16:53:28 +0000

    show an article

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'articles', to: 'articles#index'
+  get 'new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: :article
 end
