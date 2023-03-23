0980c517536738c348b2f2b3b98e8eff5ed2d749
Robert Low <robert.low91@gmail.com>
2023-03-23 16:14:27 +0000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Clearing database...'
+Article.destroy_all
+
+puts 'Generating articles...'
+
+10.times do
+  Article.create(
+    title: Faker::Music.album,
+    content: Faker::TvShows::BojackHorseman.tongue_twister
+  )
+end
+
+puts "Done, #{Article.count} articles have been created."
