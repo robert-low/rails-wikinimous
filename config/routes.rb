d5bb39bfbe14388db24eddea30d1b095c821cf87
Robert Low <robert.low91@gmail.com>
2023-03-23 17:50:09 +0000

    edit and create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new'
+
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles', to: 'articles#create'
+
   get 'articles/:id', to: 'articles#show', as: :article
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy' #To do
 end
